55feb534406fd71adb80081709ce8d2912ad0ce1
salehos <salehos763@gmail.com>
2021-01-13 12:54:29 +0330

    Working with Guilds

--- bot.py
+++ bot.py
@@ -5,12 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-print(TOKEN)
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    # print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+    	if guild.name == GUILD:
+    		print(guild.name)
+    	print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    	)
 
 client.run(TOKEN)
