1585bf24c9450c09da5ee77b1e2899d588de2c16
salehos <salehos763@gmail.com>
2021-01-14 18:17:34 +0330

    Fix group 1 and group 2 responses and Add list.txt for requests

--- bot.py
+++ bot.py
@@ -6,6 +6,14 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+def has_been_delevered (message):
+    response = "your message has been delivered"
+    await message.channel.send(response)
+
+def reserved_before(message):
+    response = "you reserved a request before"
+    await message.channel.send(response)
+
 
 intents = discord.Intents.default()
 intents.members = True
@@ -14,19 +22,49 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    print(guild.members)
     for member in guild.members:
         print(member.name)
     print(f'{client.user.name} has connected to Discord!')
 
+
 @client.event
 async def on_message(message):
+    myList = open("list.txt", "r+")
+    request_list = myList.read()
+    myList.close()
+    print(request_list)
     if message.author == client.user:
         return
-    if "99!" in message.content:
-        pm = message.content
-        response = "your message has been delivered"
-        print(pm)
-        await message.channel.send(response)
+    print(message.channel)
+    if (str(message.channel) == "group-1") and ("request" in str(message.content)):
+        if "group-1" not in request_list :
+            messages = message.content + "    " +  "group = group-1\n"
+            myList = open("list.txt" , "a+")
+            myList.write(messages)
+            myList.flush()
+            myList.close()
+            has_been_delevered(message)
+        else :
+            reserved_before(message)
+
+
+    elif (str(message.channel) == "group-2") and ("request" in str(message.content)):
+        if "group-2" not in request_list:
+            messages = message.content + "    " + "group = group-2\n"
+            myList = open("list.txt", "a+")
+            myList.write(messages)
+            myList.flush()
+            myList.close()
+            has_been_delevered(message)
+        else:
+            reserved_before(message)
+    print("_______________________________________ ")
+
+
+
+
+
 @client.event
 async def on_member_join(member):
     print("worked")
